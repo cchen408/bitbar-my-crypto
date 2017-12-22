7207846eb977f9bb74bbe25c74f0ce4542e412b5
Christopher Chen <chrichen@paypal.com>
2017-12-22 15:47:54 -0800

    holdings can have multiple

--- my-crypto/holdings.js
+++ my-crypto/holdings.js
@@ -1,31 +1,44 @@
 module.exports = [
     {
-        "name": "bitcoin",
-        "amount": 0.00467184
+        name: "bitcoin",
+        amount: 0.007038,
+        cost: 100
     },
     {
-        "name": "ethereum",
-        "amount": 0.06452843,
-        "cost": 50
+        name: "ethereum",
+        amount: 0.1489254,
+        cost: 100
     },
     {
-        "name": "ethereum",
-        "amount": 0.096363233
+        name: "litecoin",
+        amount: 0.36826107,
+        cost: 100
     },
     {
-        "name": "bitcoin-cash",
-        "amount": 0.00750913
+        name: "bitcoin",
+        amount: 0.00467184
     },
     {
-        "name": "cardano",
-        "amount": 100.89900000,
-        "cost": 50
+        name: "ethereum",
+        amount: 0.06452843,
+        cost: 50
     },
     {
-        "name": "litecoin",
-        "amount": 0.16406248,
-        "cost": 50
+        name: "ethereum",
+        amount: 0.096363233
+    },
+    {
+        name: "bitcoin-cash",
+        amount: 0.00750913
+    },
+    {
+        name: "cardano",
+        amount: 100.89900000,
+        cost: 50
+    },
+    {
+        name: "litecoin",
+        amount: 0.16406248,
+        cost: 50
     }
 ]
-
-
