f712cbb7b1617f6b4fde6d6d9dddbce0ae5f2860
Christopher Chen <chrichen@paypal.com>
2017-12-22 16:20:44 -0800

    readme updates and some formatting updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bitbar-my-crypto
 bitbar plugin for my crypto
+
+Setup
+---
+* run $```npm install``` in the command line
+* run $```chmod 755 my-crypto.js```
+* update ```holdings.js``` with your holdings
+* move the directories ```my-crypto``` ```node_modules``` along with the files ```my-crypto.js``` to your plugins directory
+
+Screenshots
+---
+![chris](https://raw.githubusercontent.com/cchen408/bitbar-my-crypto/master/screenshots/my-crypto.png)
--- my-crypto.js
+++ my-crypto.js
@@ -5,6 +5,8 @@ var request = require('request-promise');
 var icons = require('./my-crypto/icons');
 var holdings = require('./my-crypto/holdings')
 var totalValue = 0;
+var valueWithCost = 0;
+var totalCost = 0;
 const font = 'Monaco';
 
 async function myCrypto() {
@@ -33,6 +35,13 @@ async function myCrypto() {
         // add to total value
         totalValue = totalValue + value;
 
+        // add to value with cost if there is cost
+        if (!_.isNil(data.cost)) {
+            valueWithCost = valueWithCost + value;
+        }
+
+        totalCost = data.cost ? totalCost + data.cost : totalCost;
+
         // add prices to data
         data.price = cmcDatum.price_usd;
         data.symbol = cmcDatum.symbol;
@@ -49,14 +58,14 @@ async function myCrypto() {
 
     console.log(`| image=${icons.btc}`);
     console.log('---');
-    console.log(`total: ${_.round(totalValue, 2)} | color='#000' font=${font}`);
-    console.log('---');
-    console.log(`${'sym'.padEnd(5)}\t${'price'.padEnd(8)}\t${'value'.padEnd(10)}\t${'cost'.padEnd(4)}\tport% | color='#000' font=${font}`);
+    console.log(`${'sym'.padEnd(8)}\t${'price'.padEnd(8)}\t${'value'.padEnd(8)}\t${'cost'.padEnd(4)}\t% | color='#000' font=${font}`);
     console.log('---');
     _.each(holdings, function (data) {
         data.cost = data.cost || 0;
-        console.log(`${data.symbol.padEnd(5)}\t$${_.round(data.price, 5).toString().padEnd(8)}\t$${_.round(data.value, 2).toString().padEnd(10)}\t$${_.round(data.cost, 2).toString().padEnd(4)}\t${_.round(data.percentage, 2)}% | color='#000' font=${font} image=${icons[_.toLower(data.symbol)]}`);
+        console.log(`${data.symbol.padEnd(5)}\t$${_.round(data.price, 5).toString().padEnd(8)}\t$${_.round(data.value, 2).toString().padEnd(8)}\t$${_.round(data.cost, 2).toString().padEnd(4)}\t${_.round(data.percentage, 2)}% | color='#000' font=${font} image=${icons[_.toLower(data.symbol)]}`);
     });
+    console.log('---');
+    console.log(`total: $${_.round(totalValue, 2)}\t\t% profit/loss: ${_.round(valueWithCost/totalCost*100-100, 2)}% | color='#000' font=${font}`);
 }
 
 myCrypto();
